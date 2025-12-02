32676e0f456f7065812e06329036309655b60875
Melih <mlhypc@gmail.com>
2025-12-02 20:07:46 +0300

    Fix CORS: Add PATCH method to allowed methods
    - Survey submission was failing due to PATCH not being allowed
    - Added PATCH to CORS methods array

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const corsOptions = {
     process.env.FRONTEND_URL
   ].filter(Boolean),
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
